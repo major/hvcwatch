70814e8f0cc567c05d53b12e9965eb002a77689f
Major Hayden <major@mhtx.net>
2025-12-01 17:06:47 -0600

    Force loguru colors

--- src/hvcwatch/logging.py
+++ src/hvcwatch/logging.py
@@ -4,8 +4,9 @@ import sys
 
 from loguru import logger
 
-# Remove default DEBUG handler and set INFO level
+# Remove default DEBUG handler and set INFO level with forced colors
+# colorize=True forces ANSI colors even without TTY (needed for k8s/stern)
 logger.remove()
-logger.add(sys.stderr, level="INFO")
+logger.add(sys.stderr, level="INFO", colorize=True)
 
 __all__ = ["logger"]
